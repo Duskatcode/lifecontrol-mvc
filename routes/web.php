195aa8e380c0bd2e9b541e15bf530bd0a85fab8b
Dusk <jecg050505@gmail.com>
2026-05-29 21:00:46 -0500

    fix: restore survey question routes

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@ use App\Http\Controllers\EventController;
 use App\Http\Controllers\RecipeController;
 use App\Http\Controllers\MemoryGameController;
 use App\Http\Controllers\SurveyController;
+use App\Http\Controllers\SurveyQuestionController;
+use App\Http\Controllers\SurveyResponseController;
 use App\Http\Controllers\StopwatchController;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
@@ -41,6 +43,32 @@ Route::resource('recipes', RecipeController::class);
 
 Route::get('/memory-game', [MemoryGameController::class, 'index'])
     ->name('memory.index');
+
+/*
+|--------------------------------------------------------------------------
+| Survey custom routes
+|--------------------------------------------------------------------------
+| Estas rutas deben ir ANTES de Route::resource('surveys', ...)
+| para evitar conflictos con /surveys/{survey}
+*/
+
+Route::get('/surveys/{survey}/questions/create', [SurveyQuestionController::class, 'create'])
+    ->name('surveys.questions.create');
+
+Route::post('/surveys/{survey}/questions', [SurveyQuestionController::class, 'store'])
+    ->name('surveys.questions.store');
+
+Route::delete('/survey-questions/{question}', [SurveyQuestionController::class, 'destroy'])
+    ->name('survey-questions.destroy');
+
+Route::get('/surveys/{survey}/respond', [SurveyResponseController::class, 'create'])
+    ->name('surveys.respond');
+
+Route::post('/surveys/{survey}/respond', [SurveyResponseController::class, 'store'])
+    ->name('surveys.responses.store');
+
+Route::get('/surveys/{survey}/results', [SurveyResponseController::class, 'results'])
+    ->name('surveys.results');
 
 Route::resource('surveys', SurveyController::class);
 
